aa8b98d1a09c51898052d02b1685dcaf58dba27a
Theino <theino8@yahoo.com>
2018-09-15 19:24:50 -0400

    new api key

--- FirebaseFunctions/functions/index.js
+++ FirebaseFunctions/functions/index.js
@@ -2,6 +2,8 @@
 const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
+
+const google = require('@
 admin.initializeApp();
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
@@ -14,6 +16,23 @@ exports.translate = functions.https.onRequest((req, res) => {
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
     });
+    /*
+    gapi.client.init({
+          'apiKey': 'AIzaSyD1o-JutApo-Kp_CLOFnkUrgn4df5y-KT8',
+          'discoveryDocs': ['https://translation.googleapis.com/$discovery/rest?version=v2'],
+        }).then(function() {
+          // Executes an API request, and returns a Promise.
+          // The method name `language.translations.list` comes from the API discovery.
+          return gapi.client.language.translations.list({
+            q: 'hospital',
+            source: 'en',
+            target: 'de',
+          });
+        }).then(function(response) {
+          console.log(response.result.data.translations[0].translatedText);
+        }, function(reason) {
+          console.log('Error: ' + reason.result.error.message);
+        });*/
     // Push the new message into the Realtime Database using the Firebase Admin SDK.
     /*
     let correctChat = this.props.chats.find(
