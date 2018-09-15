bab9d66e9fe4188d9f6f4de5948c613e6829fd97
Theino <theino8@yahoo.com>
2018-09-15 19:55:35 -0400

    debug

--- FirebaseFunctions/functions/index.js
+++ FirebaseFunctions/functions/index.js
@@ -2,17 +2,15 @@
 const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
-
-const {Translate} = require('@google-cloud/translate');
-
+const {
+    Translate
+} = require('@google-cloud/translate');
 // Your Google Cloud Platform project ID
 const projectId = 'shellchat2018';
-
 // Instantiates a client
 const translate = new Translate({
-  projectId: projectId,
-});
-
+    projectId: projectId
+, });
 admin.initializeApp();
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
@@ -20,26 +18,26 @@ exports.translate = functions.https.onRequest((req, res) => {
     const language = req.query.language;
     const id = req.query.id;
     console.log("ver 2");
-    Translate.gapi.client.init({
-          'apiKey': 'AIzaSyB4ZpaKaLtxoQcCdU_FCBS9SrTw1tzvxE0',
-          'discoveryDocs': ['https://translation.googleapis.com/$discovery/rest?version=v2'],
-        }).then(function() {
-          // Executes an API request, and returns a Promise.
-          // The method name `language.translations.list` comes from the API discovery.
-          gapi.client.language.translations.list({
-            q: 'hospital',
-            source: 'en',
-            target: 'de',
-          });
-        }).then(function(response) {
-          console.log(response.result.data.translations[0].translatedText);
-        }, function(reason) {
-          console.log('Error: ' + reason.result.error.message);
+    gapi.client.init({
+        'apiKey': 'AIzaSyB4ZpaKaLtxoQcCdU_FCBS9SrTw1tzvxE0'
+        , 'discoveryDocs': ['https://translation.googleapis.com/$discovery/rest?version=v2']
+    , }).then(function () {
+        // Executes an API request, and returns a Promise.
+        // The method name `language.translations.list` comes from the API discovery.
+        return gapi.client.language.translations.list({
+            q: 'hospital'
+            , source: 'en'
+            , target: 'de'
+        , });
+    }).then(function (response) {
+        console.log(response.result.data.translations[0].translatedText);
+        let chatsRef = admin.database().ref('chats').on("value", function (snapshot) {
+            return res.status(200).send(snapshot.val());
+        }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
         });
-    let chatsRef = admin.database().ref('chats').on("value", function (snapshot) {
-        return res.status(200).send(snapshot.val());
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
+    }, function (reason) {
+        console.log('Error: ' + reason.result.error.message);
     });
     /*
     gapi.client.init({
