6290d52f675d18b7ae3c7a97bf5b1622f81f239e
Theino <theino8@yahoo.com>
2018-09-15 16:12:07 -0400

    making method take message

--- chat/src/ChatWindow/ChatWindow.jsx
+++ chat/src/ChatWindow/ChatWindow.jsx
@@ -9,7 +9,7 @@ class ChatHeader extends React.Component {
   }
 }
 
-function start() {
+function start(message) {
     gapi.client.init({
           'apiKey': 'AIzaSyB4ZpaKaLtxoQcCdU_FCBS9SrTw1tzvxE0',
           'discoveryDocs': ['https://translation.googleapis.com/$discovery/rest?version=v2'],
@@ -17,12 +17,13 @@ function start() {
           // Executes an API request, and returns a Promise.
           // The method name `language.translations.list` comes from the API discovery.
           return gapi.client.language.translations.list({
-            q: 'hospital',
+            q: message,
             source: 'en',
             target: 'de',
           });
         }).then(function(response) {
           console.log(response.result.data.translations[0].translatedText);
+          return response.result.data.translations[0].translatedText;
         }, function(reason) {
           console.log('Error: ' + reason.result.error.message);
         });
