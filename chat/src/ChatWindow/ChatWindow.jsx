b09fe39c9866b87d6e40922db63c4f770021b337
Theino <theino8@yahoo.com>
2018-09-15 16:09:32 -0400

    transferring stuff

--- chat/src/ChatWindow/ChatWindow.jsx
+++ chat/src/ChatWindow/ChatWindow.jsx
@@ -9,6 +9,25 @@ class ChatHeader extends React.Component {
   }
 }
 
+function start() {
+    gapi.client.init({
+          'apiKey': 'AIzaSyB4ZpaKaLtxoQcCdU_FCBS9SrTw1tzvxE0',
+          'discoveryDocs': ['https://translation.googleapis.com/$discovery/rest?version=v2'],
+        }).then(function() {
+          // Executes an API request, and returns a Promise.
+          // The method name `language.translations.list` comes from the API discovery.
+          return gapi.client.language.translations.list({
+            q: 'hospital',
+            source: 'en',
+            target: 'de',
+          });
+        }).then(function(response) {
+          console.log(response.result.data.translations[0].translatedText);
+        }, function(reason) {
+          console.log('Error: ' + reason.result.error.message);
+        });
+}
+
 // chat messages content area
 class ContentArea extends React.Component {
   render() {
