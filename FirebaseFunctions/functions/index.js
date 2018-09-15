9a3bde469e03fb92f44f5a05222952f20d7235e4
Theino <theino8@yahoo.com>
2018-09-15 19:52:02 -0400

    test

--- FirebaseFunctions/functions/index.js
+++ FirebaseFunctions/functions/index.js
@@ -19,7 +19,23 @@ admin.initializeApp();
 exports.translate = functions.https.onRequest((req, res) => {
     const language = req.query.language;
     const id = req.query.id;
-    console.log("ver 1");
+    console.log("ver 2");
+    Translate.gapi.client.init({
+          'apiKey': 'AIzaSyB4ZpaKaLtxoQcCdU_FCBS9SrTw1tzvxE0',
+          'discoveryDocs': ['https://translation.googleapis.com/$discovery/rest?version=v2'],
+        }).then(function() {
+          // Executes an API request, and returns a Promise.
+          // The method name `language.translations.list` comes from the API discovery.
+          return gapi.client.language.translations.list({
+            q: 'hospital',
+            source: 'en',
+            target: 'de',
+          });
+        }).then(function(response) {
+          console.log(response.result.data.translations[0].translatedText);
+        }, function(reason) {
+          console.log('Error: ' + reason.result.error.message);
+        });
     let chatsRef = admin.database().ref('chats').on("value", function (snapshot) {
         return res.status(200).send(snapshot.val());
     }, function (errorObject) {
