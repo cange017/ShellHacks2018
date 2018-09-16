6485f927b8c1d377482e20e1dd193278c2a3ef4c
Theino <theino8@yahoo.com>
2018-09-16 02:37:18 -0400

    a

--- chat/src/ChatWindow/ChatWindow.jsx
+++ chat/src/ChatWindow/ChatWindow.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+async function getTranslation(originalText, target) {
+  console.log('originalText', originalText);
+  console.log('target', target);
+  let url = `https://translation.googleapis.com/language/translate/v2?key=AIzaSyDj8r082goFM0L51LrF55UKJInRtX5G0UA&q=${originalText}&target=${target}`;
+  let result = await fetch(url, { method: 'POST' }).then(res => res.json().result.data.translations[0].translatedText).catch(err => ({}))
+  console.log('result', result)
+  return result;
+}
+
 // chat user's name
 class ChatHeader extends React.Component {
   render() {
@@ -52,7 +61,7 @@ class ContentArea extends React.Component {
                   message.from === window.localStorage.username ? 'me' : 'them'
                 }
               >
-                <span>{message.content}</span>
+                <span>{getTranslation(message.content, 'es')}</span>
               </li>
             ))}
         </div>
