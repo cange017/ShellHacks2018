27f60e49c8b1b96b4f41b2fdc8306bb647a4f270
Theino <theino8@yahoo.com>
2018-09-15 20:38:06 -0400

    test

--- FirebaseFunctions/functions/index.js
+++ FirebaseFunctions/functions/index.js
@@ -18,11 +18,31 @@ exports.translate = functions.https.onRequest((req, res) => {
     const language = req.query.language;
     const id = req.query.id;
     console.log("ver 3");
-    let chatsRef = admin.database().ref('chats').on("value", function (snapshot) {
-        return res.status(200).send(snapshot.val());
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-    });
+    
+    gapi.client.init({
+          'apiKey': 'AIzaSyD1o-JutApo-Kp_CLOFnkUrgn4df5y-KT8',
+          'discoveryDocs': ['https://translation.googleapis.com/$discovery/rest?version=v2'],
+        }).then(function() {
+          // Executes an API request, and returns a Promise.
+          // The method name `language.translations.list` comes from the API discovery.
+            let chatsRef = admin.database().ref('chats').on("value", function (snapshot) {
+                return res.status(200).send(snapshot.val());
+            }, function (errorObject) {
+                console.log("The read failed: " + errorObject.code);
+            });
+
+          return gapi.client.language.translations.list({
+            q: 'hospital',
+            source: 'en',
+            target: 'de',
+          });
+        }).then(function(response) {
+          console.log(response.result.data.translations[0].translatedText);
+        }, function(reason) {
+          console.log('Error: ' + reason.result.error.message);
+        });
+    
+    
     /*
     gapi.client.init({
           'apiKey': 'AIzaSyD1o-JutApo-Kp_CLOFnkUrgn4df5y-KT8',
