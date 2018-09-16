5079771910feedf95729576bb8899284d5000dc7
Theino <theino8@yahoo.com>
2018-09-16 02:44:40 -0400

    a

--- chat/src/ChatWindow/ChatWindow.jsx
+++ chat/src/ChatWindow/ChatWindow.jsx
@@ -4,7 +4,7 @@ function getTranslation(originalText, target) {
   console.log('originalText', originalText);
   console.log('target', target);
   let url = `https://translation.googleapis.com/language/translate/v2?key=AIzaSyDj8r082goFM0L51LrF55UKJInRtX5G0UA&q=${originalText}&target=${target}`;
-  let result = await fetch(url, { method: 'POST' }).then(res => res.json().result.data.translations[0].translatedText).catch(err => ({}))
+  let result = fetch(url, { method: 'POST' }).then(res => res.json().result.data.translations[0].translatedText).catch(err => ({}))
   console.log('result', result)
   return result;
 }
