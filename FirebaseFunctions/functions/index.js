ea8dfff46c73fd241087e54833414e8ad0f583fd
Theino <theino8@yahoo.com>
2018-09-15 22:15:16 -0400

    stuff

--- FirebaseFunctions/functions/index.js
+++ FirebaseFunctions/functions/index.js
@@ -2,35 +2,23 @@
 const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
-const Translate = require('@google-cloud/translate');
 // Your Google Cloud Platform project ID
 const projectId = 'shellchat2018';
+var gcloud = require('google-cloud');
 
-const translate = new Translate({
-        projectId: projectId
-    , });
+var gct = gcloud.translate({projectId: 'shellchat2018', keyFilename: 'keyfile.json'});
+admin.initializeApp();
 // Instantiates a client
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
 exports.translate = functions.https.onRequest((req, res) => {
-    admin.initializeApp();
+    
+    
+    
     const language = req.query.language;
     const id = req.query.id;
     console.log("ver 6");
     
-    // The text to translate
-    const text = 'Hello, world!';
-    // The target language
-    const target = 'ru';
-    console.log(translate);
-    // Translates some text into Russian
-    translate.translate(text, target).then(results => {
-        const translation = results[0];
-        console.log(`Text: ${text}`);
-        console.log(`Translation: ${translation}`);
-        return res.status(200).send('none');
-    }).catch(err => {
-        console.error('ERROR:', err);
-    });
+    
     
 });
