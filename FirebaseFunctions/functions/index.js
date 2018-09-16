88a5c1885d5119eb08e1de37418d860848104db9
Theino <theino8@yahoo.com>
2018-09-15 21:15:44 -0400

    delete stuff

--- FirebaseFunctions/functions/index.js
+++ FirebaseFunctions/functions/index.js
@@ -15,58 +15,7 @@ admin.initializeApp();
 exports.translate = functions.https.onRequest((req, res) => {
     const language = req.query.language;
     const id = req.query.id;
-    console.log("ver 4");
+    console.log("ver 5");
     
-    return res.status(200).send(Translate.gapi.client.init({
-          'apiKey': 'AIzaSyD1o-JutApo-Kp_CLOFnkUrgn4df5y-KT8',
-          'discoveryDocs': ['https://translation.googleapis.com/$discovery/rest?version=v2'],
-        }).then(function() {
-          // Executes an API request, and returns a Promise.
-          // The method name `language.translations.list` comes from the API discovery.
-            /*let message = 'error';
-            let chatsRef = admin.database().ref('chats').on("value", function (snapshot) {
-                return res.status(200).send(snapshot.val());
-            }, function (errorObject) {
-                console.log("The read failed: " + errorObject.code);
-            });
-*/
-          return gapi.client.language.translations.list({
-            q: 'hospital',
-            source: 'en',
-            target: 'de',
-          });
-        }).then(function(response) {
-            
-          console.log(response.result.data.translations[0].translatedText);
-            return 'junk';
-        }, function(reason) {
-          console.log('Error: ' + reason.result.error.message);
-        }));
-    
-    //return res.status(200).send('nothing');
-    
-    /*
-    gapi.client.init({
-          'apiKey': 'AIzaSyD1o-JutApo-Kp_CLOFnkUrgn4df5y-KT8',
-          'discoveryDocs': ['https://translation.googleapis.com/$discovery/rest?version=v2'],
-        }).then(function() {
-          // Executes an API request, and returns a Promise.
-          // The method name `language.translations.list` comes from the API discovery.
-          return gapi.client.language.translations.list({
-            q: 'hospital',
-            source: 'en',
-            target: 'de',
-          });
-        }).then(function(response) {
-          console.log(response.result.data.translations[0].translatedText);
-        }, function(reason) {
-          console.log('Error: ' + reason.result.error.message);
-        });*/
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    /*
-    let correctChat = this.props.chats.find(
-      chat => id === this.props.selectedChat
-    );
-    // Grab the text parameter.
-    */
+    return res.status(200).send('none');
 });
